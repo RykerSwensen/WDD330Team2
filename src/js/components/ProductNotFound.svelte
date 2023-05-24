e3da39f7e3a68c2e528934c0af072feb2ea851dd
Tyler242 <77693574+Tyler242@users.noreply.github.com>
2023-05-24 11:07:42 -0600

    refactor product-details + added product not found

---
+++ src/js/components/ProductNotFound.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<section>
+  <h2>Unable to find product</h2>
+  <a href="/index.html">Return Home</a>
+</section>
+
+<style>
+  section {
+    margin-bottom: 10px;
+  }
+</style>
